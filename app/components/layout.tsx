5fb005c138eb6d2f90278b961a0fe9a87e3146a0
Nikos <printezisn@gmail.com>
2022-05-27 22:26:24 +0300

    Add initial layout

---
+++ app/components/layout.tsx
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+import { FC, ReactNode } from 'react';
+
+interface Props {
+  pageTitle: string,
+  pageDescription: string,
+  children?: ReactNode
+}
+
+const Layout: FC<Props> = ({ pageTitle, pageDescription, children }) => (
+  <>
+    <Head>
+      <title>{pageTitle} | Game Solver</title>
+      <meta name="description" content={pageDescription} />
+      <link rel="icon" href="/favicon.png" />
+    </Head>
+    <header>
+      <h1 className="sr-only">{pageTitle}</h1>
+    </header>
+    <main>
+      <div className="container">
+        {children}
+      </div>
+    </main>
+    <footer>
+      <p className="container">
+        <a
+          href="https://www.flaticon.com/free-icons/cube"
+          title="cube icons"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Cube icons created by Good Ware - Flaticon
+        </a>
+      </p>
+    </footer>
+  </>
+);
+
+export default Layout;
